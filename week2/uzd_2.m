ac1bd639710b6ab79ebb47fe1eb14019cdecc6c8
RubenisR <113258677+RubenisR@users.noreply.github.com>
2022-12-03 13:13:19 +0100

    tets

--- week2/uzd_2.m
+++ week2/uzd_2.m
@@ -1,11 +1,17 @@
 n=0:4;
 subplot(3,1,1);
 x1=0.5*cos(((10*pi)/7)*n+(1/4)*pi);
-stem(x1,n)
+stem(n,x1)
 
 subplot(3,1,2);
-x2=0.3*cos(((20*pi)/7)*n);%why are 2 values the same?
-stem(x2,n)
+x2=0.3*cos(((20*pi)/7)*n);
+stem(n,x2)
+
+subplot(3,1,3);
+x3=x1+x2;
+stem(n,x3)
+
+
 
 
 
