62dad0f6ccd5c4ddd4dd6da1d13ce71ce58a092e
RubenisR <113258677+RubenisR@users.noreply.github.com>
2022-12-12 14:46:18 +0100

    Fixed week2

--- week2/uzd2_part2.m
+++ week2/uzd2_part2.m
@@ -1,13 +1,20 @@
-subplot(2,1,1); %mirror
+
 n=-10:10;
+i=find((n>=0)&(n<=4));
+i=1:length(n);
+
 x1=0.5*cos(((10*pi)/7)*n+(1/4)*pi);
 x2=0.3*cos(((20*pi)/7)*n);
-x4=x1+x2;
-stem(n(11:15),x4(11:15))
+x3=x1+x2;
 
+x4=x3(end:-1:1);
+for k=3:length(n)
+    x5(k)=x4(k-2);
+end
+
+subplot(2,1,1); %mirror
+stem(n(i),x4(i))
 subplot(2,1,2); %delay
-%n=((0:4)*(-1)+2);
-x1=0.5*cos(((10*pi)/7)*n+2+(1/4)*pi);
-x2=0.3*cos(((20*pi)/7)*n);
-x5=x1+x2;
-stem(n(11:15),x5(11:15))
+stem(n(i),x5(i))
+
+
